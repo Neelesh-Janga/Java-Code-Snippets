cccdf88ca5d36991152c9f76aa2d3120f94912fc
Neelesh Janga <codelikeneelesh@gmail.com>
2024-07-14 23:25:09 +0530

    ReadWrite Lock Practical

---
+++ src/dev/neelesh/multithreading/producer_consumer_problem/readwritelock/SharedResourceImpl.java
@@ -0,0 +1,78 @@
+package dev.neelesh.multithreading.producer_consumer_problem.readwritelock;
+
+import dev.neelesh.multithreading.producer_consumer_problem.SharedResource;
+
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class SharedResourceImpl implements SharedResource {
+    
+    private Queue<Integer> queue;
+    private final int MAX_SIZE = 5;
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+//    private Condition readLockCondition = lock.readLock().newCondition(); // This is not possible
+    private Condition writeLockCondition = lock.writeLock().newCondition();
+    
+    public SharedResourceImpl(Queue<Integer> queue) {
+        this.queue = queue;
+    }
+    
+    public void produceData(int data){
+        lock.writeLock().lock();
+        try {
+            while (queue.size() == MAX_SIZE)
+                writeLockCondition.await();
+            queue.add(data);
+            
+            if(data % 10 == 0) modTen(); // Reentrancy example
+            
+            System.out.println("ReadWriteLock :: " + Thread.currentThread().getName() + " produced : " + data);
+            writeLockCondition.signalAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            lock.writeLock().unlock();
+        }
+    }
+    
+    public void consumeData() {
+        lock.writeLock().lock();
+        try {
+            while(queue.isEmpty()) writeLockCondition.await();
+            System.out.println("ReadWriteLock :: " + Thread.currentThread().getName() + " consumed : " + queue.poll());
+            readAllContents();
+            writeLockCondition.signalAll();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            lock.writeLock().unlock();
+        }
+    }
+    
+    public void modTen(){
+        lock.writeLock().lock();
+        try {
+            System.out.println("Mod 10 value will be inserted by " + Thread.currentThread().getName() + " 👇🏼");
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            lock.writeLock().unlock();
+        }
+    }
+    
+    public void readAllContents(){
+        lock.readLock().lock();
+        try {
+            System.out.printf(
+                    "%s printing the Queue: %s\n",
+                    Thread.currentThread().getName(), queue
+            );
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            lock.readLock().unlock();
+        }
+    }
+}
---
+++ src/dev/neelesh/multithreading/producer_consumer_problem/readwritelock/ThreadsPractical.java
@@ -0,0 +1,32 @@
+package dev.neelesh.multithreading.producer_consumer_problem.readwritelock;
+
+
+import dev.neelesh.multithreading.producer_consumer_problem.Consumer;
+import dev.neelesh.multithreading.producer_consumer_problem.Producer;
+import dev.neelesh.multithreading.producer_consumer_problem.SharedResource;
+
+import java.util.ArrayDeque;
+import java.util.Random;
+
+public class ThreadsPractical {
+
+    public static void main(String[] args) {
+        readwriteLockPractical();
+    }
+    
+    private static void readwriteLockPractical(){
+        SharedResource resource = new SharedResourceImpl(new ArrayDeque<>());
+        Random random = new Random();
+
+        Thread producer_1 = new Thread(new Producer(resource, random, "ReadWriteLock"), "Producer 1");
+        Thread consumer_1 = new Thread(new Consumer(resource, "ReadWriteLock"), "Consumer 1");
+
+        Thread producer_2 = new Thread(new Producer(resource, random, "ReadWriteLock"), "Producer 2");
+        Thread consumer_2 = new Thread(new Consumer(resource, "ReadWriteLock"), "Consumer 2");
+
+        producer_1.start();
+        producer_2.start();
+        consumer_1.start();
+        consumer_2.start();
+    }
+}
